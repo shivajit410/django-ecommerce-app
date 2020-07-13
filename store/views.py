98a1b25016ba5ce7ed7c3b78329c45904608a833
Shivaji Thattikota <shivajithattikota@outlook.com>
2020-07-13 15:01:38 +0530

    Render checkout page dynamically

--- store/views.py
+++ store/views.py
@@ -21,5 +21,13 @@ def cart(request):
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # Create empty cart for non logged users
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        items = []
+    context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
