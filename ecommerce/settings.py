daa9713759124403c700232554d5557db636fa5b
Shivaji Thattikota <shivajithattikota@outlook.com>
2020-07-14 14:11:16 +0530

    Testing S3 integration

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -132,3 +132,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_STORAGE_ACCESS_KEY = os.environ.get('AWS_STORAGE_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
